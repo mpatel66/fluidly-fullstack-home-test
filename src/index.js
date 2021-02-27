4e895978bc7c9d21ce51961bb0ef9ec290503846
Meera <meera.patel2984@gmail.com>
2021-02-27 11:42:40 +0000

    Feat: add tests and body of assertEquals function as in requirements list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 module.exports = function assertEquals(expected, actual) {
+
+  if (Array.isArray(expected)) {
+    if (!Array.isArray(actual)) throw new Error(`Expected type array but found type ${typeof actual}`);
+    else if (expected.length !== actual.length) throw new Error(`Expected array length ${expected.length} but found ${actual.length}`);
+    else {
+      const match = expected.every((item, index) => actual[index] === item);
+      if (!match) throw new Error(`Expected ${expected} but found ${actual}`);
+      return match;
+    }
+  }
+
+  if (expected !== actual) throw new Error(`Expected ${expected} but found ${actual}`);
+
+
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -37,4 +37,21 @@ describe('assertEquals', () => {
   it('doesnt throw when 2 strings that are equal are compared', () => {
     expect(() => assertEquals('abc', 'abc')).not.toThrow()
   })
+
+  it('throws an error when 2 unequal strings are compared', () => {
+    expect(() => assertEquals('abcef', 'abc')).toThrow('Expected abcef but found abc')
+  })
+
+  it('throws an error when array and object are compared', () => {
+    expect(() => assertEquals(['a'], {0:'a'})).toThrow('Expected type array but found type object')
+  })
+
+  it('doesn\'t throw an error when 2 arrays that are equal are compared', () => {
+    expect(() => assertEquals(['a','b','c'], ['a','b','c'])).not.toThrow()
+  })
+
+  it('throws an error when 2 arrays of unequal length are compared', () => {
+    expect(() => assertEquals(['a','b'], ['a','b','c'])).toThrow('Expected array length 2 but found 3');
+  })
+
 })
